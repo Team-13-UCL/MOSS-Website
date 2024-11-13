f5859e15c478f5e13f4efcd77c14de44b91da7c0
Minikbusk <minikbusk@hotmail.com>
2024-11-13 08:17:44 +0100

    small changes

--- MOSS-Website/src/components/Footer.jsx
+++ MOSS-Website/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-red-500">Footer</div>
+    <div className="p-4 shadow-lg">Footer</div>
   )
 }
 
--- MOSS-Website/src/components/Navbar.jsx
+++ MOSS-Website/src/components/Navbar.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-
-// export default function NavBar() {
-//     return (
-//         <>
-//             <nav>
-//                 <div className='nav-links'>
-//                     <NavLink to="/">
-//                         <img
-//                             src="../assets/MOSS_Logo.png"
-//                             alt="MOSS Logo"
-//                             className="nav-logo"
-//                         />
-//                     </NavLink>
-//                     <NavLink to="/dit-besoeg">Dit besøg</NavLink>
-//                     <NavLink to="/skoletjenesten">Skoletjenesten</NavLink>
-//                     <NavLink to="/om-museet">Om museet</NavLink>
-//                     <NavLink to="/kontakt">Kontakt os</NavLink>
-//                 </div>
-//                 <div className='nav-museums'>
-//                     <NavLink to="/dorf-moellegaard">Dorf Møllegård</NavLink>
-//                     <NavLink to="/vildmosemuseet">Vildmosemuseet</NavLink>
-//                 </div>
-//             </nav>
-//         </>
-//     );
-// }
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
